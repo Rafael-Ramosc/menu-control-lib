1f4c5ce6bad3e8533124051bc96ca2686d9f6018
Rafael-Ramosc <rafael.ramosrc@gmail.com>
2024-11-22 17:56:23 -0300

    feat: create a utils file to use crossterm

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,5 @@
 // A module for the navigate and render.
 pub mod navigate;
+
+// A module for the utils to abstract crossterm functions. -> this explanation make sense?
+pub mod utils;
---
+++ src/utils.rs
@@ -0,0 +1,33 @@
+use crossterm::{
+    cursor::{self, MoveTo},
+    execute,
+    style::{Color, Print, SetForegroundColor},
+    terminal,
+};
+use std::io::stdout;
+
+pub fn clear_terminal() {
+    let mut stdout = stdout();
+    execute!(
+        stdout,
+        terminal::Clear(terminal::ClearType::All),
+        cursor::MoveTo(0, 0)
+    )
+    .expect("Error cleaning terminal");
+}
+
+// TODO: test this function
+pub fn print_at(x: u16, y: u16, text: &str) -> std::io::Result<()> {
+    execute!(stdout(), MoveTo(x, y), Print(text))
+}
+
+// TODO: test this function
+pub fn print_at_colored(x: u16, y: u16, text: &str, color: Color) -> std::io::Result<()> {
+    execute!(
+        stdout(),
+        MoveTo(x, y),
+        SetForegroundColor(color),
+        Print(text),
+        SetForegroundColor(Color::Reset)
+    )
+}
